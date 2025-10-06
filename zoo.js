41a3a19bf30f94dc121716b68e46f14ef1ec7983
Ywj Siab Vaj <yvaj@students.ntc.edu>
2025-10-06 16:22:02 -0500

    Fixed index, manifest, and zoo

--- zoo.js
+++ zoo.js
@@ -174,7 +174,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.groupEnd();
 
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/Sprint-C3-Zoo/serviceworker.js')
+      navigator.serviceWorker.register('/serviceworker.js')
       .then(() => console.log("Service Worker registered"))
       .catch((err) => console.error("SW registration failed", err));
     }
